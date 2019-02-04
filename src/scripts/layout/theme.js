c0612af31027dccf08abff73476235d6cd138cbd
mikewalker24 <mikewalker24@gmail.com>
2019-02-04 00:38:42 -0500

    merge

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -22,3 +22,45 @@ if (cookiesEnabled()) {
     'supports-cookies',
   );
 }
+
+// Quantity buttons
+
+const $changeQuantity = $('.js-change-quantity');
+
+if ($changeQuantity) {
+  $changeQuantity.on('click', function () {
+    const $this = $(this);
+    const $input = $(this).siblings('input');
+    const val = parseInt($input.val());
+    let valMax = 999;
+    const valMin = $input.attr('min') || 0;
+
+    if ($input.attr('max') != null) {
+      valMax = $input.attr('max');
+    }
+
+    if (isNaN(val) || val < valMin) {
+      $input.val(valMin);
+      return false;
+    } else if (val > valMax) {
+      $input.val(valMax);
+      return false;
+    }
+
+    if ($this.data('func') === 'plus') {
+      if (val < valMax) {
+        $input.val(val + 1);
+      }
+    } else {
+      if (val > valMin) {
+        $input.val(val - 1);
+      }
+      if ($this.parents('.cart_item').length) {
+        if (val - 1 === 0) {
+          $this.closest('.cart_item').addClass('animated fadeOutUp');
+        }
+      }
+    }
+    $input.trigger('change');
+    return false;
+  });
